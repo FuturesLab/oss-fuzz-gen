31f651dd08a81faf6a18b44cec90d09db91e24fb
gabe-sherman <gabesherman6@gmail.com>
2026-02-25 12:48:54 -0700

    modified ci runner to run fuzzing trials

---
+++ report/docker_run_fuzz.py
@@ -0,0 +1,194 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Usage:
+  docker_run.py [options]
+"""
+
+import argparse
+import datetime
+import logging
+import os
+import subprocess
+import sys
+
+# Configure logging to display all messages at or above INFO level
+logging.basicConfig(level=logging.INFO)
+
+BENCHMARK_SET = 'comparison'
+FREQUENCY_LABEL = 'daily'
+RUN_TIMEOUT = 300
+SUB_DIR = 'default'
+MODEL = 'vertex_ai_gemini-1-5'
+DELAY = 0
+NUM_SAMPLES = 10
+LLM_FIX_LIMIT = 5
+MAX_ROUND = 100
+DATA_DIR = '/experiment/data-dir/'
+BUCKET_NAME = 'oss-fuzz-gcb-experiment-run-logs'
+
+
+def _parse_args(cmd) -> argparse.Namespace:
+  """Parses the command line arguments."""
+  parser = argparse.ArgumentParser(description='Run experiments')
+  parser.add_argument(
+      '-pj',
+      '--project',
+      default='',
+      help='The name of the project to run fuzzing trials for')
+
+  parser.add_argument(
+      '-a',
+      '--approach',
+      default=['ofg', 'bluebird_ofg', 'promefuzz', 'bluebird_promefuzz'],
+      nargs="+",
+      help='Fuzz the harnesses for each given approach')
+
+  parser.add_argument(
+      '-nt',
+      '--num-trials',
+      default=1,
+      help='The number of fuzzing trials to run')
+
+  parser.add_argument(
+      '-ps',
+      '--pool-size',
+      default=8,
+      help='The number of fuzzing trials to run concurrently')
+
+  parser.add_argument(
+      '-l',
+      '--frequency-label',
+      type=str,
+      default=FREQUENCY_LABEL,
+      help=(f'Used as part of Cloud Build tags and GCS report directory, '
+            f'default: {FREQUENCY_LABEL}.'))
+                    
+  parser.add_argument(
+      '-to',
+      '--run-timeout',
+      type=int,
+      default=RUN_TIMEOUT,
+      help=f'Fuzzing timeout in seconds, default: {RUN_TIMEOUT} seconds.')
+
+  parser.add_argument(
+      '-rd',
+      '--redirect-outs',
+      type=str,
+      default="false",
+      help=
+      'Redirects experiments stdout/stderr to file. Set to "true" to enable.')
+
+  args, additional_args = parser.parse_known_args(cmd)
+
+  # Arguments after the first element ("--") separator.
+  args.additional_args = additional_args[1:]
+
+  return args
+
+
+def _run_command(command: list[str], shell=False):
+  """Runs a command and return its exit code."""
+  process = subprocess.run(command, shell=shell, check=False)
+  return process.returncode
+
+
+def _authorize_gcloud():
+  """Authorizes to gcloud"""
+  # When running the docker container locally we need to activate the service
+  # account from the env variable.
+  # When running on GCP this step is unnecessary.
+  google_creds = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '')
+  if google_creds:
+    logging.info("GOOGLE APPLICATION CREDENTIALS set: %s.", google_creds)
+    _run_command([
+        'gcloud', 'auth', 'activate-service-account',
+        'LLM-EVAL@oss-fuzz.iam.gserviceaccount.com', '--key-file', google_creds
+    ])
+  else:
+    # TODO: Set GOOGLE_APPLICATION_CREDENTIALS and ensure cloud build uses it.
+    logging.info("GOOGLE APPLICATION CREDENTIALS is not set.")
+
+
+def _log_common_args(args):
+  """Prints args useful for logging"""
+  logging.info("Target project is %s.", args.project)
+  logging.info("Harnesses for approach %s is being fuzzed.", ', '.join(args.approach))
+  logging.info("Frequency label is %s.", args.frequency_label)
+  logging.info("Fuzzing for a total of %s(s).", args.run_timeout)
+  logging.info("Num trials: %s.", args.num_trials)
+  logging.info("Pool size: %s.", args.pool_size)
+
+
+def main(cmd=None):
+  """Main entrypoint"""
+  run_fuzz_trial(cmd)
+
+def run_fuzz_trial(cmd=None):
+  """Run fuzzing trials on supplied harnesses."""
+  args = _parse_args(cmd)
+
+  # Uses python3 by default and /venv/bin/python3 for Docker containers.
+  python_path = "/venv/bin/python3" if os.path.exists(
+      "/venv/bin/python3") else "python3"
+  os.environ["PYTHON"] = python_path
+
+  _authorize_gcloud()
+  _log_common_args(args)
+
+  date = datetime.datetime.now().strftime('%Y-%m-%d')
+  local_results_dir = 'results'
+  os.makedirs(local_results_dir, exist_ok=True)
+
+  # Experiment name is used to label the Cloud Builds and as part of the
+  # GCS directory that build logs are stored in.
+  #
+  # Example directory: 2023-12-02-daily-comparison
+  experiment_name = f"{date}-{args.frequency_label}-{args.project}"
+
+  # Prepare the command to run experiments
+  run_cmd = [
+    python_path, "run_driver.py", 
+    "--project", args.project,
+    "--approach", *args.approach,
+    "--fuzz-timeout", str(args.run_timeout), 
+    "--num-trials", str(args.num_trials), 
+    "--pool-size", str(args.pool_size), 
+    "--cloud-experiment-name", experiment_name,
+    "--cloud-experiment-bucket", "oss-fuzz-gcb-experiment-run-logs",
+    "--results-dir", local_results_dir,
+  ]
+
+  # Run the experiment and redirect to file if indicated.
+  if args.redirect_outs:
+    with open(f"{local_results_dir}/logs-from-run.txt", "w") as outfile:
+      process = subprocess.run(run_cmd,
+                               stdout=outfile,
+                               stderr=outfile,
+                               check=False)
+      ret_val = process.returncode
+  else:
+    process = subprocess.run(run_cmd, check=False)
+    ret_val = process.returncode
+
+  os.environ["ret_val"] = str(ret_val)
+
+  with open("/experiment_ended", "w"):
+    pass
+
+  # Exit with the return value of `./run_all_experiments`.
+  return ret_val
+
+if __name__ == "__main__":
+  sys.exit(main())
--- run_driver.py
+++ run_driver.py
@@ -15,8 +15,6 @@ import experiment.builder_runner as builder_runner_lib
 """Build and run a fuzzing trial for a given approach. Runtime length and number of trials can be provided as arguments"""
 
 logger = logging.getLogger(__name__)
-FILE_DIR = Path(__file__).resolve().parent
-OSS_PATH = os.path.expanduser("~/oss-fuzz-bluebird")
 BUILDER_RUNNER_TAG = "ghcr.io/gabe-sherman/oss-fuzz-base-runner:latest"
 
 
@@ -30,6 +28,41 @@ POOL_SIZE = 8
 oss_fuzz_checkout.ENABLE_CACHING = False
 LOG_FMT = ('%(asctime)s.%(msecs)03d %(levelname)s '
            '%(module)s - %(funcName)s: %(message)s')
+
+def parse_args():
+    global NUM_TRIALS, APPROACHES, RUN_TIMEOUT
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--project", type=str, required=True)
+    parser.add_argument("-a", "--approach", type=str, choices=APPROACHES, nargs="+")
+    parser.add_argument("-ft", "--fuzz-timeout", type=int)
+    parser.add_argument("-nt", "--num-trials", type=int)
+    parser.add_argument("-ps", "--pool-size", type=int)
+    parser.add_argument("-r", "--results-dir", type=str, default = None)
+    parser.add_argument('-c',
+                      '--cloud-experiment-name',
+                      type=str,
+                      default='',
+                      help='The name of the cloud experiment.')
+    parser.add_argument('-cb',
+                        '--cloud-experiment-bucket',
+                        type=str,
+                        help='A gcloud bucket to store experiment files.')
+    
+    args = parser.parse_args()
+
+    if args.approach:
+        APPROACHES = args.approach
+
+    if args.num_trials:
+        NUM_TRIALS = args.num_trials
+
+    if args.fuzz_timeout:
+        RUN_TIMEOUT = args.fuzz_timeout
+
+    if args.pool_size:
+        POOL_SIZE = args.pool_size
+
+    return args
 
 def check_custom_runner() -> None:
     inspect_cmd = ["docker", "image", "inspect", BUILDER_RUNNER_TAG]
@@ -77,45 +110,44 @@ def setup_evaluator(args, benchmark, workdir, approach):
 
     return evaluator
 
-def parse_args():
-    global NUM_TRIALS, APPROACHES, RUN_TIMEOUT
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--project", type=str, required=True)
-    parser.add_argument("-a", "--approach", type=str, choices=APPROACHES, nargs="+")
-    parser.add_argument("-ft", "--fuzz-timeout", type=int)
-    parser.add_argument("-nt", "--num-trials", type=int)
-    parser.add_argument("-ps", "--pool-size", type=int)
-    parser.add_argument('-c',
-                      '--cloud-experiment-name',
-                      type=str,
-                      default='',
-                      help='The name of the cloud experiment.')
-    parser.add_argument('-cb',
-                        '--cloud-experiment-bucket',
-                        type=str,
-                        help='A gcloud bucket to store experiment files.')
-    
-    args = parser.parse_args()
+def clone_custom_oss(dest_path: str) -> bool:
+    command = [
+        "git",
+        "clone",
+        "https://github.com/FuturesLab/oss-fuzz-bluebird.git",
+        dest_path
+    ]
 
-    if args.approach:
-        APPROACHES = args.approach
+    result = sp.run(
+        command,
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+        text=True,
+        timeout=300,
+        check=False,
+    )
 
-    if args.num_trials:
-        NUM_TRIALS = args.num_trials
+    if result.returncode != 0:
+        logger.error("Git clone failed with return code %d", result.returncode)
+        logger.error("STDOUT:\n%s", result.stdout)
+        logger.error("STDERR:\n%s", result.stderr)
+        return False
 
-    if args.fuzz_timeout:
-        RUN_TIMEOUT = args.fuzz_timeout
+    return True
 
-    if args.pool_size:
-        POOL_SIZE = args.pool_size
-
-    return args
+def setup_logger():
+    logging.basicConfig(
+      level="INFO",
+      format=LOG_FMT,
+      datefmt='%Y-%m-%d %H:%M:%S',
+    )
+    logging.getLogger('').setLevel("INFO")
 
 def run_fuzz_trial(args: argparse.Namespace, workdir_base: str, appr: str, trial: int):
     """Spawn the fuzzing campaign with a specific trial and approach identifier"""
     try:
-        source_dir = os.path.join(FILE_DIR, "target_src", args.project, appr)
-        build_script_path = os.path.join(FILE_DIR, "target_src", args.project, "build.sh")
+        source_dir = os.path.join("fuzzing_trials", "target_src", args.project, appr)
+        build_script_path = os.path.join("fuzzing_trials", "target_src", args.project, "build.sh")
         test_name = f"{args.project}-{appr}-{trial}".lower()
         workdir = WorkDirs(os.path.join(workdir_base, f'output-{test_name}'))
         evaluator = setup_evaluator(args, benchmark, workdir, appr)
@@ -134,24 +166,26 @@ def run_fuzz_trial(args: argparse.Namespace, workdir_base: str, appr: str, trial
     
 if __name__ == "__main__":
     args = parse_args()
-    logging.basicConfig(
-      level="INFO",
-      format=LOG_FMT,
-      datefmt='%Y-%m-%d %H:%M:%S',
-    )
-    # Set the base logger level
-    logging.getLogger('').setLevel("INFO")
+    setup_logger()
 
+    # Get the target benchmark yaml
     benchmarks = Benchmark.from_yaml(os.path.join("targets", f"{args.project}.yaml"))
     benchmark = benchmarks[0]
-    copy_oss_path = os.path.join(FILE_DIR, "oss-fuzz")
-    shutil.copytree(OSS_PATH, copy_oss_path, dirs_exist_ok=True, ignore=shutil.ignore_patterns(".git"))
-    oss_fuzz_checkout.OSS_FUZZ_DIR = copy_oss_path
-    oss_fuzz_checkout.postprocess_oss_fuzz()
-    # oss_fuzz_checkout.clone_oss_fuzz(os.path.join(FILE_DIR, "oss-fuzz"))
-    check_custom_runner()
-    workdir_base = os.path.join(FILE_DIR, f"results-{args.project}")
 
+    # Set up custom oss-fuzz repo
+    oss_path = "oss-fuzz"
+    if not os.path.exists(oss_path):
+        clone_custom_oss(oss_path)
+    oss_fuzz_checkout.OSS_FUZZ_DIR = oss_path
+    oss_fuzz_checkout.postprocess_oss_fuzz()
+    if not args.cloud_experiment_name:
+        check_custom_runner()
+
+    # Set up result path
+    result_dir = args.results_dir if args.results_dir else f"results-{args.project}"
+    workdir_base = result_dir
+
+    # Begin fuzzing trials
     experiment_tasks = []
     with Pool(POOL_SIZE, maxtasksperchild=1) as p:
         for trial in range(0, NUM_TRIALS):
@@ -160,8 +194,5 @@ if __name__ == "__main__":
                 experiment_tasks.append(fuzz_task)
 
         experiment_results = [task.get() for task in experiment_tasks]
-        # Signal that no more work will be submitte to the pool.
         p.close()
-
-        # Wait for all workers to complete.
         p.join()
